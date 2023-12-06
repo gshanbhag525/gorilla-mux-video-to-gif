f24a2cb53d9c6c41ff1477e3dd283d777fb76538
Gunesh Shanbhag <gunesh@crunchmediaworks.com>
2023-12-06 18:52:15 +0530

    create a gif from video

--- main.go
+++ main.go
@@ -4,71 +4,50 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"os/exec"
 
 	"github.com/gorilla/mux"
-	"github.com/jmoiron/sqlx"
-	_ "github.com/mattn/go-sqlite3"
 )
 
-// Model represents a sample data structure for demonstration
-type Model struct {
-	ID   int    `db:"id"`
-	Name string `db:"name"`
-}
-
 func main() {
-	// Dependency management tools: Godep, Sltr, etc. (not used in this example)
-
-	// Go templating language (not used in this example)
-
-	// Go code generation tools, such as Stringer (not used in this example)
-
-	// Popular Go web frameworks/libraries
-	// Router packages: Gorilla Mux
 	r := mux.NewRouter()
 
-	// SQL and NoSQL database setup
-	db, err := sqlx.Open("sqlite3", ":memory:")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer db.Close()
+	r.HandleFunc("/convert-to-gif", func(w http.ResponseWriter, r *http.Request) {
+		// Replace "path/to/your/input/video.mp4" with the actual path to your video file
+		inputVideoPath := "./video.mp4"
+		// Replace "path/to/your/output/output.gif" with the desired output GIF file path
+		outputGifPath := "./output.gif"
 
-	// Create a table for demonstration
-	_, err = db.Exec(`
-		CREATE TABLE IF NOT EXISTS models (
-			id INTEGER PRIMARY KEY,
-			name TEXT
-		)
-	`)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Common Goroutine and channel patterns (not used in this example)
-
-	// MVC, RESTful, ORM
-	r.HandleFunc("/models", func(w http.ResponseWriter, r *http.Request) {
-		// Sample endpoint demonstrating CRUD operations with an ORM (sqlx)
-		var models []Model
-		err := db.Select(&models, "SELECT * FROM models")
+		err := convertVideoToGif(inputVideoPath, outputGifPath)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
 
-		// Display models
-		fmt.Fprintf(w, "Models: %+v", models)
+		fmt.Fprintf(w, "Video successfully converted to GIF.")
 	}).Methods("GET")
 
-	// Basic experience with Reactjs (not used in this example)
-
-	// Code versioning tools: Git (not used in this example)
-
-	// Basic knowledge of macOS and terminal commands (not used in this example)
-
-	// Start the server
 	http.Handle("/", r)
 	fmt.Println("Server is running on :8080...")
 	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func convertVideoToGif(inputVideoPath, outputGifPath string) error {
+	// Check if the input video file exists
+	if _, err := os.Stat(inputVideoPath); os.IsNotExist(err) {
+		return fmt.Errorf("Input video file does not exist")
+	}
+
+	// Use ffmpeg to convert the video to gif
+	cmd := exec.Command("ffmpeg", "-i", inputVideoPath, "-vf", "fps=10,scale=320:-1", outputGifPath)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	err := cmd.Run()
+	if err != nil {
+		return fmt.Errorf("Error converting video to GIF: %v", err)
+	}
+
+	return nil
 }
