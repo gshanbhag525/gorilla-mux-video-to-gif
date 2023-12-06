cb92fe2bccc8b47d00783f7b243d94aa46c7ab5f
Gunesh Shanbhag <gunesh@crunchmediaworks.com>
2023-12-06 19:07:26 +0530

    multippart uplaod

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"os"
@@ -14,19 +15,53 @@ func main() {
 	r := mux.NewRouter()
 
 	r.HandleFunc("/convert-to-gif", func(w http.ResponseWriter, r *http.Request) {
-		// Replace "path/to/your/input/video.mp4" with the actual path to your video file
-		inputVideoPath := "./video.mp4"
-		// Replace "path/to/your/output/output.gif" with the desired output GIF file path
-		outputGifPath := "./output.gif"
+		// Parse the form data to get the file
+		err := r.ParseMultipartForm(10 << 20) // 10 MB limit
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
 
-		err := convertVideoToGif(inputVideoPath, outputGifPath)
+		file, _, err := r.FormFile("video")
+		if err != nil {
+			http.Error(w, "Error retrieving the file", http.StatusBadRequest)
+			return
+		}
+		defer file.Close()
+
+		// Save the uploaded file
+		uploadPath := "./uploads/"
+		err = os.MkdirAll(uploadPath, os.ModePerm)
+		if err != nil {
+			http.Error(w, "Error creating upload directory", http.StatusInternalServerError)
+			return
+		}
+
+		fileName := "uploaded_video.mp4"
+		filePath := uploadPath + fileName
+		out, err := os.Create(filePath)
+		if err != nil {
+			http.Error(w, "Error creating the file", http.StatusInternalServerError)
+			return
+		}
+		defer out.Close()
+
+		_, err = io.Copy(out, file)
+		if err != nil {
+			http.Error(w, "Error saving the file", http.StatusInternalServerError)
+			return
+		}
+
+		// Convert the uploaded video to GIF
+		outputGifPath := "./output.gif"
+		err = convertVideoToGif(filePath, outputGifPath)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
 
 		fmt.Fprintf(w, "Video successfully converted to GIF.")
-	}).Methods("GET")
+	}).Methods("POST")
 
 	http.Handle("/", r)
 	fmt.Println("Server is running on :8080...")
